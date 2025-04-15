c5f81416af3db7e2cb2c3c2929d964dee95d1631
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2025-04-15 10:25:51 +0200

    fix: Run `npm update`

--- regenerate.sh
+++ regenerate.sh
@@ -5,3 +5,4 @@ openapi-generator generate \
   -g typescript-axios \
   --git-repo-id api-client-ts --git-user-id digiseg-labs \
   --additional-properties=npmName=digiseg-api-client,npmVersion=0.0.0-dev,supportsES6=true
+npm update
